fb1dbcaf71958a223891ba3cea03df252fab0822
YunheeAhn <ayh95944674@gmail.com>
2025-11-02 20:12:32 +0900

    feat : 연락처 구성

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { Grid } from "@mui/material";
 import "./App.css";
+import ContactForm from "./component/ContactForm";
+import ContactList from "./component/ContactList";
+import Navbar from "./component/Navbar";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <section className="contents">
+        <div className="inner add-contact">
+          <ContactForm />
+        </div>
+        <div className="inner contact-list">
+          <ContactList />
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default App;
